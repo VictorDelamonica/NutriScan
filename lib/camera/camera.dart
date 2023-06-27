56008a9cea3db0ff68848a469df06be2925c9a20
Victor Delamonica <victor.delamonica@epitech.eu>
2023-06-27 19:02:00 +0200

    Add text extractor

--- lib/camera/camera.dart
+++ lib/camera/camera.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nutriscan/Utilities/appBar.dart';
+import 'package:http/http.dart' as http;
 
 import '../Pages/Home/home.dart';
 
@@ -46,8 +48,8 @@ class _CameraPageState extends State<CameraPage> {
           context,
           MaterialPageRoute(
               builder: (context) => PreviewPage(
-                picture: picture,
-              )));
+                    picture: picture,
+                  )));
     } on CameraException catch (e) {
       debugPrint('Error occured while taking picture: $e');
       return null;
@@ -71,49 +73,49 @@ class _CameraPageState extends State<CameraPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: Stack(children: [
-            (_cameraController.value.isInitialized)
-                ? CameraPreview(_cameraController)
-                : Container(
+      child: Stack(children: [
+        (_cameraController.value.isInitialized)
+            ? CameraPreview(_cameraController)
+            : Container(
                 color: Colors.black,
                 child: const Center(child: CircularProgressIndicator())),
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.20,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
-                      color: Colors.black),
-                  child:
+        Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.20,
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+                  color: Colors.black),
+              child:
                   Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-                    Expanded(
-                        child: IconButton(
-                          padding: EdgeInsets.zero,
-                          iconSize: 30,
-                          icon: Icon(
-                              _isRearCameraSelected
-                                  ? CupertinoIcons.switch_camera
-                                  : CupertinoIcons.switch_camera_solid,
-                              color: Colors.white),
-                          onPressed: () {
-                            setState(
-                                    () => _isRearCameraSelected = !_isRearCameraSelected);
-                            initCamera(widget.cameras![_isRearCameraSelected ? 0 : 1]);
-                          },
-                        )),
-                    Expanded(
-                        child: IconButton(
-                          onPressed: takePicture,
-                          iconSize: 50,
-                          padding: EdgeInsets.zero,
-                          constraints: const BoxConstraints(),
-                          icon: const Icon(Icons.circle, color: Colors.white),
-                        )),
-                    const Spacer(),
-                  ]),
+                Expanded(
+                    child: IconButton(
+                  padding: EdgeInsets.zero,
+                  iconSize: 30,
+                  icon: Icon(
+                      _isRearCameraSelected
+                          ? CupertinoIcons.switch_camera
+                          : CupertinoIcons.switch_camera_solid,
+                      color: Colors.white),
+                  onPressed: () {
+                    setState(
+                        () => _isRearCameraSelected = !_isRearCameraSelected);
+                    initCamera(widget.cameras![_isRearCameraSelected ? 0 : 1]);
+                  },
                 )),
-          ]),
-        ));
+                Expanded(
+                    child: IconButton(
+                  onPressed: takePicture,
+                  iconSize: 50,
+                  padding: EdgeInsets.zero,
+                  constraints: const BoxConstraints(),
+                  icon: const Icon(Icons.circle, color: Colors.white),
+                )),
+                const Spacer(),
+              ]),
+            )),
+      ]),
+    ));
   }
 }
 
@@ -122,18 +124,63 @@ class PreviewPage extends StatelessWidget {
 
   final XFile picture;
 
+  Future<String> extractTextFromImage() async {
+    final url = Uri.parse('https://api.ocr.space/parse/image');
+    List<int> imageBytes = await picture.readAsBytes();
+    String base64Image = 'data:image/jpeg;base64,${base64Encode(imageBytes)}';
+
+    final response = await http.post(url, body: {
+      'base64Image': base64Image,
+      'apikey': "K89661829688957",
+    });
+
+    if (response.statusCode == 200) {
+      final json = response.body;
+      final parsedJson = jsonDecode(json);
+      if (parsedJson['ParsedResults'] is List<dynamic>) {
+        final parsedResults = parsedJson['ParsedResults'] as List<dynamic>;
+        if (parsedResults.isNotEmpty) {
+          final parsedText = parsedResults[0]['ParsedText'] as String;
+          return parsedText;
+        }
+      }
+      throw Exception('Failed to extract text from image');
+    } else {
+      throw Exception(
+          'Failed to extract text from image: ${response.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar("Preview", onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const HomePage()));
       }),
-      body: Center(
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Image.file(File(picture.path), fit: BoxFit.cover, width: 250),
-          const SizedBox(height: 24),
-          Text(picture.name)
-        ]),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.file(File(picture.path), fit: BoxFit.cover, width: 250),
+              const SizedBox(height: 24),
+              FutureBuilder<String>(
+                future: extractTextFromImage(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final extractedText = snapshot.data!;
+                    return Text("Text: ${extractedText.split(' ')}");
+                  } else if (snapshot.hasError) {
+                    return Text("Error: ${snapshot.error}");
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
